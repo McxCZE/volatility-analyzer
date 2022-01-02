783bae83f5c8ece1e31e429ec67bf897988e970c
Matasx <matasx@seznam.cz>
2022-01-02 15:13:33 +0100

    Init.

---
+++ VolatilityAnalyzer/DefaultDownloader.cs
@@ -0,0 +1,36 @@
+﻿using Downloader.Core.Core;
+using Downloader.Core.Exchange.Binance;
+using Downloader.Core.Exchange.Bitfinex;
+using Downloader.Core.Exchange.FTX;
+using Downloader.Core.Exchange.Kucoin;
+using Downloader.Core.Utils;
+
+namespace VolatilityAnalyzer
+{
+    internal class DefaultDownloader
+    {
+        private readonly DownloadOrchestrator _downloadOrchestrator;
+
+        public DefaultDownloader(IProgress progress)
+        {
+            var ui = new UserInterface();
+            var client = new HttpClient(new TransientErrorRetryHttpClientHandler());
+            _downloadOrchestrator = new DownloadOrchestrator(ui, progress, new IGenericDownloader[] {
+                new BinanceDownloader(client),
+                new BitfinexDownloader(client),
+                new FTXDownloader(client),
+                new KucoinDownloader(client)
+            });
+        }
+
+        public string Download(DownloadTask downloadTask)
+        {
+            return _downloadOrchestrator.Download(downloadTask);
+        }
+
+        public async Task<IEnumerable<SymbolInfo>> GetSymbols(string exchange)
+        {
+            return await _downloadOrchestrator.GetDownloader(exchange).GetSymbolsAsync();
+        }
+    }
+}
---
+++ VolatilityAnalyzer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using Downloader.Core.Core;
+using log4net;
+
+namespace VolatilityAnalyzer
+{
+    internal class Program
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(Program));
+
+        private static async Task Main()
+        {
+            ServicePointManager.DefaultConnectionLimit = 10;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            Log.Info("Starting");
+
+            var downloader = new DefaultDownloader(new NullProgress());
+            var range = DateTimeRange.FromUtcToday(TimeSpan.FromDays(-365));
+
+            const string exchange = "KUCOIN";
+
+            var currencyPreference = new[] { "USD", "EUR", "USDT" };
+            var symbols = await downloader.GetSymbols(exchange);
+
+            var t = Array.IndexOf(currencyPreference, "USDs");
+
+            var filtered = symbols
+                .Select(x => new {Symbol = x, Order = Array.IndexOf(currencyPreference, x.Currency)})
+                .Where(x => x.Order >= 0)
+                .GroupBy(x => x.Symbol.Asset)
+                .Select(x => x.OrderBy(s => s.Order).First().Symbol)
+                .ToList();
+
+            foreach (var symbolInfo in filtered)
+            {
+                var filename = downloader.Download(new DownloadTask($"data\\{exchange}", exchange, symbolInfo.Symbol, range));
+            }
+
+            //TODO: analyze all pairs
+        }
+    }
+}
