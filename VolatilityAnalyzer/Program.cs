98ef9b59555a92070081b034665eef7afac1417a
McxCZE <milan.cihak@brouzdej.net>
2022-03-25 14:55:35 +0100

    Update Program.cs
    Total rewrite of logic, I couldnt understand it. Koko too dumb.

--- VolatilityAnalyzer/Program.cs
+++ VolatilityAnalyzer/Program.cs
@@ -17,6 +17,7 @@ namespace VolatilityAnalyzer
             Log.Info("Starting");
 
             int lookBackDays = -30;
+
             var downloader = new DefaultDownloader(new NullProgress());
             var range = DateTimeRange.FromDiff(new DateTime(2022, 1, 2, 0, 0, 0, DateTimeKind.Utc),
                 TimeSpan.FromDays(lookBackDays));
@@ -35,8 +36,7 @@ namespace VolatilityAnalyzer
                 .ToList();
 
             await using var master = File.CreateText($"{exchange}-summary.csv");
-            await master.WriteLineAsync("Asset,Currency,Stddev,Avg,DStddev,DAvg");
-
+            await master.WriteLineAsync("Asset,Currency,Oscilation");
 
             var files = filtered.Select(x => new
                 {
@@ -54,47 +54,24 @@ namespace VolatilityAnalyzer
                 var symbolInfo = context.SymbolInfo;
                 Console.WriteLine($"Processing: {symbolInfo}");
 
-                var window = (int)TimeSpan.FromDays(1).TotalMinutes;
-                var halfWindow = window / 2;
-
-                var prices = (await File.ReadAllLinesAsync(filename, token)).Select(x => double.Parse(x, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture)).ToList();
-                if (prices.Count <= window || prices.Count < minPrices) return;
-
-                //var ma = GetMovingAverages(prices, window);
-                var oscilation = GetOscilation(prices);
-
-                //var prevPerc = 0d;
-
-                var stats = prices
-                    .Skip(halfWindow)
-                    .Take(prices.Count - halfWindow)
-                    .Zip(oscilation, (price, oscilation) =>
-                    {
-                        //var perc = Math.Abs(price == 0 ? 0 : (ma - price) / price * 100);
-                        //if (perc > 100) perc *= 0.1d;
-                        //var diff = Math.Abs(prevPerc - perc);
-                        //prevPerc = perc;
-                        return new { Price = price, Oscilation = oscilation };
-                    })
+                var prices = (await File.ReadAllLinesAsync(filename, token))
+                    .Select(x => double.Parse(x, CultureInfo.InvariantCulture))
                     .ToList();
 
-                var lines = stats.Select(x =>
-                    $"{x.Price.Ts()},{x.Oscilation.Ts()}");
+                if (prices.Count <= 1) return;
 
-                await File.WriteAllLinesAsync(Path.ChangeExtension(filename, ".analyzed.csv"), lines, token);
-                //var percStat = stats.Select(x => x.Perc).Stat();
-                //var diffStat = stats.Select(x => x.Diff).Stat();
-                var oscilationStat = stats.Select(x => x.Oscilation).Stat();
+                var oscilation = GetOscilation(prices);
 
                 await semaphore.WaitAsync(token);
-                await master.WriteLineAsync($"{symbolInfo.Asset},{symbolInfo.Currency},{oscilationStat}");
+                await master.WriteLineAsync($"{symbolInfo.Asset},{symbolInfo.Currency},{oscilation}");
                 await master.FlushAsync();
                 semaphore.Release();
             });
         }
 
-        private static IEnumerable<double> GetOscilation(
-            IEnumerable<double> data
+        
+        private static double GetOscilation(
+            List<double> data
         )
         {
             var lastSgn = 0;
@@ -114,43 +91,15 @@ namespace VolatilityAnalyzer
                 lastPrice = price;
             }
 
-            yield return changedDirectionCount;
+            return changedDirectionCount;
         }
 
-        private static IEnumerable<double> GetMovingAverages(
-            IEnumerable<double> data,
-            int window
+        private static double GetMa(
+            List<double> data
         )
         {
-            var samples = new Queue<double>();
-            var runningTotal = 0d;
-
-            foreach (var price in data)
-            {
-                samples.Enqueue(price);
-                runningTotal += price;
-
-                if (samples.Count <= window) continue;
-
-                runningTotal -= samples.Dequeue();
-                yield return runningTotal / samples.Count;
-            }
-        }
-    }
-
-    public static class ExtensionClass
-    {
-        public static (double Stddev, double Average) Stat(this IEnumerable<double> sequence)
-        {
-            var values = sequence.ToList();
-            var average = values.Average();
-            var sum = values.Sum(d => Math.Pow(d - average, 2));
-            return new ValueTuple<double, double>(Math.Sqrt(sum / (values.Count - 1)), average);
-        }
-
-        public static string Ts(this double value)
-        {
-            return value.ToString(CultureInfo.InvariantCulture);
+            //dopsat.
+            return 0;
         }
     }
 }
